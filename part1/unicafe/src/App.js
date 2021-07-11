de1d86ff2f81d06d5e79e59ccade960ffcf9d871
Asiliia <asylia.t@gmail.com>
2021-07-11 12:52:56 +0300

    task: Task 1.6, add frame

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,66 @@
 import React, { useState } from 'react'
 
+const Header1 = ({header}) => {
+    return (
+        <>
+            <h1>Unicafe</h1>
+        </>
+    )
+}
+const Header2 = ({header}) => {
+    return (
+        <>
+            <h2>give feedback</h2>
+        </>
+    )
+}
+const Button = ({name}) => {
+    return (
+        <>
+            <button>{name}</button>
+        </>
+    )
+}
+const ButtonGroup = ({buttons}) => {
+    const [good, neutral, bad] = buttons
+    return (
+        <>
+            <Button name={good}/>
+            <Button name={neutral}/>
+            <Button name={bad}/>
+        </>
+    )
+}
+
+const Statistics = ({buttons}) => {
+    const [good, neutral, bad] = buttons
+    return (
+        <>
+            <p>{good} - 6</p>
+            <p>{neutral} - 2</p>
+            <p>{bad} - 1</p>
+        </>
+    )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const data = {
+      h1: 'Unicafe',
+      h2: {feedback: 'give feedback', statistics: 'statistics'},
+      buttons: ['good', 'neutral', 'bad']
+}
 
   return (
     <div>
-      code here
+      <Header1/>
+      <Header2/>
+      <ButtonGroup buttons={data.buttons}/>
+      <Header2/>
+      <Statistics buttons={data.buttons}/>
     </div>
   )
 }

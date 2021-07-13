d1f59bd50372a1a6d77af6968fbaeb617b9f76a6
Asiliia <asylia.t@gmail.com>
2021-07-13 20:15:27 +0300

    feat: Task 2.8: Add field 'phone'

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '123-456-7653' }
   ]);
-  const [ newName, setNewName ] = useState('');
+  const [ newData, setNewData ] = useState({newName: '', newPhone: ''});
 
   const isExisting = (name) => {
     return persons.some(person => person.name === name) && name !== '';
@@ -12,19 +12,25 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (isExisting(newName)) {
-      alert (`${newName} is already added to phonebook`);
-    } else {
-    const personObject = {
-      name: newName.trim()
+    if (isExisting(newData.newName) || isExisting(newData.newPhone)) {
+      alert (`${newData.newName} is already added to phonebook`);
     }
-    setPersons([...persons, personObject]);
-    setNewName('');
+    else {
+      const personObject = {
+        name: newData.newName.trim(),
+        phone: newData.newPhone.trim()
+      }
+      setPersons([...persons, personObject]);
+      setNewData({newName: '', newPhone: ''});
     }
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    setNewData({...newData, newName: event.target.value});
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewData({...newData, newPhone: event.target.value});
   }
   
   return (
@@ -32,14 +38,35 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+        <label htmlFor="name">Enter your name: </label>
+           <input
+            id="name" 
+            type="text"
+            value={newData.newName} 
+            onChange={handleNameChange}
+            placeholder="Ann Smith"
+            maxLength="30"
+            size="30"
+            required/>
         </div>
         <div>
-          <button type="submit">add</button>
+        <label htmlFor="phone">Enter your phone: </label>
+          <input 
+            id="phone"
+            type="tel" 
+            value={newData.newPhone}
+            onChange={handlePhoneChange}
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+            placeholder="123-456-7890"
+            maxLength="12"
+            required />
         </div>
+        <p>
+          <button type="submit">add</button>
+        </p>
       </form>
       <h2>Numbers</h2>
-       {persons.map(person => <div>{person.name}</div>)}
+       {persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
     </div>
   )
 }
